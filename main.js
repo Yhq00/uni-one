1654e94da2a43f845e2a721f7cca44312713491d
Yhq <1193086708@qq.com>
2023-11-01 14:17:47 +0800

    feat: 首页开发

--- main.js
+++ main.js
@@ -3,7 +3,39 @@
 import Vue from 'vue'
 import App from './App'
 
+//到入网络请求的包
+import { $http } from '@escook/request-miniprogram'
+
+uni.$http = $http
+
+$http.baseUrl = 'https://api-hmugo-web.itheima.net';
+
+
+// 请求开始之前做一些事情,请求拦截器
+$http.beforeRequest = function (options) {
+  // do somethimg...
+  uni.showLoading({
+  	title:'数据加载中...'
+  })
+}
+
+// 请求完成之后做一些事情，响应拦截器
+$http.afterRequest = function () {
+  // do something...
+  uni.hideLoading()
+}
+
 Vue.config.productionTip = false
+
+
+//统一封装请求失败提示的方法
+uni.$showMsg = function(title = '数据请求失败！',duration = 1500){
+	uni.showToast({
+		title,
+		duration,
+		icon:'none'
+	})
+}
 
 App.mpType = 'app'
 
